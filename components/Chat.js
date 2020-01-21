4cc896c291b8a99b6becc1911bf0fdce15e1b4ca
trohini31383 <rohini.tomar@gmail.com>
2020-01-21 10:26:45 -0800

    added gigted chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Platform } from "react-native";
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+import KeyboardSpacer from 'react-native-keyboard-spacer'
 export default class Chat extends React.Component {
 
   constructor() {
 
     super();
-    this.state = {}
+    this.state = {
+      messages: [],
+      user: {
+
+        _id: '',
+
+        name: '',
+
+        avatar: ''
+
+      },
+    };
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -18,6 +31,45 @@ export default class Chat extends React.Component {
     };
 
   };
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  componentWillUnmount() { }
+
+  renderBubble(props) {
+    return (
+      <Bubble {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: 'white',
+          },
+          right: {
+            backgroundColor: 'green'
+          }
+        }} />
+    )
+  }
 
   render() {
 
@@ -39,7 +91,38 @@ export default class Chat extends React.Component {
 
         }}
 
-      ></View>
+      >
+
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardSpacer /> : null}
+
+
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    flex: 1,
+
+    backgroundColor: "#fff",
+
+    alignItems: "center",
+
+    justifyContent: "center",
+
+    width: "100%"
+
+  }
+
+});
