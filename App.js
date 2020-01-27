713227cfbd56c9d669f5cee2a29062f2d54cd49b
trohini31383 <rohini.tomar@gmail.com>
2020-01-27 13:03:30 -0800

    stored message collection in database via firebase

--- App.js
+++ App.js
@@ -7,6 +7,25 @@ import Chat from './components/Chat';
 import { createAppContainer } from 'react-navigation';
 
 import { createStackNavigator } from 'react-navigation-stack';
+const firebase = require("firebase");
+
+require("firebase/firestore");
+
+this.state = {
+  lists: [],
+};
+
+firebase.initializeApp({
+
+  apiKey: "AIzaSyBRu3B4hnEjXADsP3cZgATAdl0whjkQuWg",
+  authDomain: "chat-app-ecfe3.firebaseapp.com",
+  databaseURL: "https://chat-app-ecfe3.firebaseio.com",
+  projectId: "chat-app-ecfe3",
+  storageBucket: "chat-app-ecfe3.appspot.com",
+  messagingSenderId: "199946168634",
+  appId: "1:199946168634:web:a3e0ca184cbe617c134803",
+  measurementId: "G-G1J73S34FT"
+});
 
 const navigator = createStackNavigator({
   Start: { screen: Start },
@@ -14,5 +33,7 @@ const navigator = createStackNavigator({
 });
 
 const navigatorContainer = createAppContainer(navigator);
+
+
 // Export it as the root component
 export default navigatorContainer;
--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 
-import { StyleSheet, View, Text, Platform } from "react-native";
-import { GiftedChat, Bubble } from 'react-native-gifted-chat'
-import KeyboardSpacer from 'react-native-keyboard-spacer'
+import { StyleSheet, View, Text, Platform, FlatList } from "react-native";
+import { GiftedChat, Bubble, ActionButton } from 'react-native-gifted-chat';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+import firebase from "firebase";
+
+
+import "firebase/firestore";
 export default class Chat extends React.Component {
 
   constructor() {
 
     super();
+
+    this.referenceMessageUser = null;
+    this.referenceMessages = firebase.firestore().collection('messages')
+
     this.state = {
       messages: [],
       user: {
@@ -30,38 +38,164 @@ export default class Chat extends React.Component {
 
     };
 
-  };
+  }
+
+  setUser = (_id, name = 'Guest User', avatar = 'https://placeimg.com/140/140/any') => {
+
+    this.setState({
+
+      user: {
+
+        _id: _id,
+
+        name: name,
+
+        avatar: avatar,
+
+      }
+
+    })
+
+  }
+
 
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: 'This is a system message',
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
-    })
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
+
+      if (!user) {
+
+        await firebase.auth().signInAnonymously();
+
+      }
+      if (!this.props.navigation.state.params.name) {
+
+        this.setUser(user.uid);
+
+        this.setState({
+
+          uid: user.uid,
+
+          loggedInText: "Hello there"
+
+        });
+
+      } else {
+
+        this.setUser(user.uid, this.props.navigation.state.params.name)
+
+        this.setState({
+
+          uid: user.uid,
+
+          loggedInText: "Hello there"
+
+        });
+
+      }
+      // create a reference to the active user's documents (messages)
+
+      this.referenceMessageUser = firebase.firestore().collection("messages");
+
+      // listen for collection changes for current user
+
+      this.unsubscribeMessageUser = this.referenceMessageUser.orderBy('createdAt', 'desc').onSnapshot(this.onCollectionUpdate);
+
+    });
+
   }
+
+
+  addMessage() {
+
+    console.log(this.state.user)
+
+    this.referenceMessages.add({
+
+      _id: this.state.messages[0]._id,
+
+      text: this.state.messages[0].text || '',
+
+      createdAt: this.state.messages[0].createdAt,
+
+      user: this.state.user,
+
+      uid: this.state.uid,
+
+      image: this.state.messages[0].image || '',
+
+      location: this.state.messages[0].location || null,
+
+    });
+
+  }
+
+
+
+
 
   onSend(messages = []) {
     this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+      messages: GiftedChat.append(previousState.messages, messages)
+    }),
+      () => {
+
+        this.addMessage();
+      }
+
+    );
   }
 
-  componentWillUnmount() { }
+  componentWillUnmount() {
+    // stop listening to authentication
+
+    this.authUnsubscribe();
+
+    //stop listening for changes
+
+    this.unsubscribeMessageUser();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+
+      let data = doc.data();
+
+      messages.push({
+
+        _id: data._id,
+
+        text: data.text,
+
+        createdAt: data.createdAt.toDate(),
+
+        user: data.user,
+
+        image: data.image || '',
+
+        location: data.location || null,
+
+      });
+
+    });
+
+    this.setState({
+
+      messages
+
+    });
+
+  };
+
+
+
+
+
+
+
+
 
   renderBubble(props) {
     return (
@@ -80,9 +214,9 @@ export default class Chat extends React.Component {
   render() {
 
     /**
-
+  
     * uses name and background color defiend on start screen
-
+  
     */
 
     return (
@@ -102,15 +236,19 @@ export default class Chat extends React.Component {
         <GiftedChat
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
+
           onSend={messages => this.onSend(messages)}
-          user={{
-            _id: 1,
-          }}
+
+          user={this.state.user}
         />
+
         {Platform.OS === 'android' ? <KeyboardSpacer /> : null}
 
 
       </View>
+
+
+
     )
   }
 }
